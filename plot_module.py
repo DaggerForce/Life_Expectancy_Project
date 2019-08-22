cbfd21c0763162b5aea38d7a7d31dbac67c82fa5
Gal Gilor <gilorgal2010@gmail.com>
2019-08-22 14:59:12 -0400

    remove lean feature, creates some more graphs

---
+++ plot_module.py
@@ -0,0 +1,91 @@
+def create_hist(data, transform=None, title=None, xlabel=None):
+    '''This function creates an histogram. It can also accept transformation method to 
+       create a histogram after the transoformation ('log', 'sqrt', 'cbrt')'''
+    if transform == 'log':
+        data = np.log(data)
+    elif transform == 'sqrt':
+        data = np.sqrt(data)
+    elif transform == 'cube':
+        data = np.cbrt(data)
+    # create the plot
+    plt.figure(figsize=(6, 3))
+    plt.hist(data, density=True)
+    # add the labels
+    plt.title(title)
+    plt.ylabel('Frequency')
+    plt.xlabel(xlabel)
+    plt.show
+    return
+
+
+def create_scatter(x, y, title, xlabel, ylabel, file_name):
+    '''This function creates a scatter plot and saves the plot as png on your device.'''
+
+    fig, ax = plt.subplots(figsize=(8, 5))
+    # create the scatter
+    ax.scatter(x, y, color="orange", alpha=0.5)
+
+    # adds a title and axes labels
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+
+    # removing top and right borders
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    plt.savefig(f'{file_name}.png')
+    return
+
+
+def CorrMtx(df, dropDuplicates=True, xrot=70, yrot=0, label='Variable'):
+    '''This function accepts a correlation matrix of your data, returns a heat map and save
+       the plot on your device'''
+
+    # Exclude duplicate correlations by masking uper right values
+    if dropDuplicates:
+        mask = np.zeros_like(df, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+
+    # Set background color / chart style
+    sns.set_style(style='white')
+
+    # Set up  matplotlib figure
+    f, ax = plt.subplots(figsize=(12, 10))
+
+    # Add diverging colormap from red to blue
+    cmap = sns.diverging_palette(250, 10, as_cmap=True)
+    # add titles
+    plt.title("Correlation Heat Map")
+
+    # Draw correlation plot with or without duplicates
+    if dropDuplicates:
+        sns.heatmap(df, mask=mask, cmap=cmap,
+                    square=True,
+                    linewidth=.5, cbar_kws={"shrink": .5}, ax=ax)
+        plt.xlabel(label)
+        plt.ylabel(label)
+        plt.xticks(rotation=xrot)
+        plt.yticks(rotation=yrot)
+        plt.savefig('heatmap.png')
+
+    else:
+        sns.heatmap(df, cmap=cmap,
+                    square=True,
+                    linewidth=.5, cbar_kws={"shrink": .5}, ax=ax)
+        plt.xlabel(label)
+        plt.ylabel(label)
+        plt.xticks(rotation=xrot)
+        plt.yticks(rotation=yrot)
+        plt.savefig('heatmap.png')
+    return
+
+
+def get_pairs(data, row_groups, depended, features, n, fig_name):
+    row_groups = [features[i:i+n] for i in range(0, len(features), n)]
+    # create the plots
+    for ind in row_groups:
+        plot = sns.pairplot(x_vars=ind, y_vars=depended,
+                            data=data, kind="reg", height=3)
+    # save the file
+    plt.savefig(f"{fig_name}.png')
+    return
